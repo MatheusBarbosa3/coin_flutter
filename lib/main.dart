8c7421beeb78b2d159bb8e56b278c109ac8b59ef
MatheusBarbosa3 <matheusbdos31@gmail.com>
2022-02-10 23:11:54 -0300

    using state management with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:coin_flutter/repositories/account_repository.dart';
+import 'package:coin_flutter/repositories/coin_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'my_app.dart';
 
 void main() {
-  // ignore: prefer_const_constructors
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CoinRepository()),
+        ChangeNotifierProvider(
+            create: (context) => AccountRepository(
+                  coins: context.read<CoinRepository>(),
+                )),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
